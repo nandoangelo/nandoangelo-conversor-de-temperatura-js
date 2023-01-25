be0de55dffcc625c85c0207f9ef6b647705dd279
Fernando Angelo <nandoangelo@gmail.com>
2023-01-25 15:32:30 -0300

    Atualizando comentários
    Deixando a documentação mais clara

--- js/funcionamento.js
+++ js/funcionamento.js
@@ -28,46 +28,46 @@ function converteTemperatura() {
   var simbolo = document.getElementById('simbolo');
   var res = document.getElementById('convertido');
 
-  // Forçando a conversão em número
+  // Forçando a conversão do valor do campo em número
   temperatura = Number(temperatura.value);
 
   // Identifica o símbolo da temperatura digitada pelo usuário
   // e faz a devida conversão de valores.
   if (simbolo[0].selected) {
     
-    // CELSIUS SELECIONADO
+    // => CELSIUS SELECIONADO
     
-    // Converte a temperatura em: 
-    var fahrenheit  = (temperatura * 9/5) + 32;
-    var kelvin      = temperatura + 273.15;
+    // Então converte graus celsius em... 
+    var fahrenheit = (temperatura * 9/5) + 32;
+    var kelvin = temperatura + 273.15;
 
-    // Escreve a resposta na tela
+    // ... escreve a resposta no espaço reservado
     res.innerHTML = `<p><strong>${temperatura} °C</strong> equivalem a:</p>`;
     res.innerHTML += `<p><strong>${fahrenheit.toFixed(2)} °F</strong> (Fahrenheit)</p>`;
     res.innerHTML += `<p><strong>${kelvin.toFixed(2)} K</strong> (Kelvin)</p>`;
 
   } else if (simbolo[1].selected) {
     
-    // FAHRENHEIT SELECIONADO
+    // => FAHRENHEIT SELECIONADO
     
-    // Converte a temperatura em: 
-    var celsius     = (temperatura - 32) * 5/9;
-    var kelvin      = (temperatura - 32) * 5/9 + 273.15;
+    // Então converte graus fahrenheit em...
+    var celsius = (temperatura - 32) * 5/9;
+    var kelvin = (temperatura - 32) * 5/9 + 273.15;
 
-    // Escreve a resposta na tela
+    // ... escreve a resposta no espaço reservado
     res.innerHTML = `<p><strong>${temperatura} °F</strong> equivalem a:</p>`;
     res.innerHTML += `<p><strong>${celsius.toFixed(2)} °C</strong> (Celsius)</p>`;
     res.innerHTML += `<p><strong>${kelvin.toFixed(2)} K</strong> (Kelvin)</p>`;
 
   } else if (simbolo[2].selected) {
     
-    // KELVIN SELECIONADO
+    // => KELVIN SELECIONADO
     
-    // Converte a temperatura em: 
-    var celsius     = temperatura - 273.15;
-    var fahrenheit  = (temperatura - 273.15) * 9/5 + 32;
+    // Então converte kelvin para ... 
+    var celsius = temperatura - 273.15;
+    var fahrenheit = (temperatura - 273.15) * 9/5 + 32;
 
-    // Escreve a resposta na tela
+    // ... escreve a resposta no espaço reservado
     res.innerHTML = `<p><strong>${temperatura} K</strong> equivalem a:</p>`;
     res.innerHTML += `<p><strong>${celsius.toFixed(2)} °C</strong> (Celsius)</p>`;
     res.innerHTML += `<p><strong>${fahrenheit.toFixed(2)} °F</strong> (Fahrenheit)</p>`;
